3dbefa68c728eb7c25bc075f00f1b1e44a55c157
Rob Weltman <robw@worldspot.com>
2025-12-03 17:28:52 -1000

    Lower case keys on query returns

--- src/DBBase.php
+++ src/DBBase.php
@@ -92,17 +92,19 @@ abstract class DBBase {
 		} else {
 			$dsn = "mysql:host={$config['host']};port={$config['port']};dbname={$config['dbname']};charset={$charset}";
 		}
+		//echo "dsn: $dsn, {$config['username']}, {$config['password']}\n";
 		
 		$options = [
 			PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
 			PDO::ATTR_EMULATE_PREPARES => false,
+			PDO::ATTR_CASE => PDO::CASE_LOWER, 
 		];
 		
 		try {
 			return new PDO($dsn, $config['username'], $config['password'], $options);
 		} catch (PDOException $e) {
-			throw new Exception('Database connection failed: ' . $e->getMessage());
+			throw new Exception('Database connection failed: ' . $e->getMessage() . $dsn);
 		}
 	}
 	
